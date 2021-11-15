e8966abe3e9400c68abb8649da57598b53e5d7c3
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-15 09:29:38 -0500

    part 11 infinite queries

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+-- Create db
 CREATE DATABASE social_media;
 
 CREATE TABLE users (
@@ -12,3 +13,17 @@ CREATE TABLE posts (
     body VARCHAR(255) NOT NULL,
     author_id INT references users(id) NOT NULL
 );
+
+-- Submit new post
+INSERT INTO posts (body, author_id) VALUES ($1, $2);
+
+-- Get feed
+SELECT u.username, u.img, p.body FROM users u 
+INNER JOIN posts p ON u.id = p.author_id 
+ORDER BY p.id DESC LIMIT 5 OFFSET {min}
+
+-- Get my posts
+SELECT u.username, u.img, p.body FROM users u 
+INNER JOIN posts p ON u.id = p.author_id 
+WHERE p.author_id = u.id 
+ORDER BY p.id DESC LIMIT 5 OFFSET {min}
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,4 +19,21 @@ router.post("/new_post", isAuth, async (req, res) => {
   res.status(200).send();
 });
 
+router.get("/feed", isAuth, async (req, res) => {
+  const cursor = req.query.cursor;
+  const posts = await pool.query(
+    "SELECT u.username, u.img, p.body FROM users u INNER JOIN posts p ON u.id = p.author_id ORDER BY p.id DESC LIMIT 5 OFFSET $1",
+    [cursor]
+  );
+  res.send({ cursor: cursor * 1 + 5, posts: posts.rows });
+});
+
+router.get("/my_posts", isAuth, async (req, res) => {
+  const cursor = req.query.cursor;
+  const posts = await pool.query(
+    "SELECT u.username, u.img, p.body FROM users u INNER JOIN posts p ON u.id = p.author_id WHERE p.author_id = $1 ORDER BY p.id DESC LIMIT 5 OFFSET $2",
+    [req.user.id, cursor]
+  );
+  res.send({ cursor: cursor * 1 + 5, posts: posts.rows });
+});
 module.exports = router;
